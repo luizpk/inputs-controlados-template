724ab069df341b638e73d9c1fb4d55bad95eb0fc
LuizPK <luizpkpinheiro@gmail.com>
2022-12-14 21:20:01 -0300

    Resolvendo exercícios criando useState e usando onChange para controlar os dados inseridos no input

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,18 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {MainContainer, Form, Input} from './styles'
 
 function MainPage() {
+
+  // Aplicando useState
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [email, setEmail] = useState('');
+
+  // Aplicando onChange
+
+  const inputName = (event) => {
+    setName(event.target.value)
+  };
+
+  const inputAge = (event) => {
+    setAge(event.target.value)
+  };
+
+  const inputEmail = (event) => {
+    setEmail(event.target.value)
+  }
+
+  // Recuperando e imprimindo os dados
+
+  const dataPrint = () => {
+    console.log(`Nome: ${name}
+    Idade: ${age}
+    E-mail: ${email}`);
+
+    setName('');
+    setAge('');
+    setEmail('');
+  }
+
   return (
     <MainContainer>
       <h2>Formulário de inscrição</h2>
       <Form>
         <label>
           Nome:
-          <Input />
+          <Input placeholder='Nome' onChange={inputName}  value={name}/>
         </label>
         <label>
           Idade:
-          <Input/>
+          <Input placeholder='Idade' onChange={inputAge} value={age}/>
+        </label>  
+        <label>
+          E-mail:
+          <Input placeholder = "E-mail" onChange={inpuEmail} value={email}/>
+        </label>  
       <button>Enviar dados</button>
       </Form>
     </MainContainer>
